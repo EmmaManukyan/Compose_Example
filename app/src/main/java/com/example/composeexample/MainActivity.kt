07b9006aec4202b1fcc56b4071d4f6ee58c19cc0
Veqya <vemir.amirkhanyan@gmail.com>
2023-07-23 11:16:10 +0400

    compose ui first example

---
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.composeexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeexample.ui.theme.ComposeExampleTheme
+import com.example.composeexample.ui.theme.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+        }
+    }
+}
